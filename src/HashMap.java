724cc32dec5dee94a75a8e04ea5399d72ff15a70
tifa2UP <tifa3333@gmail.com>
2016-09-28 17:16:03 -0700

    Initial commit

---
+++ src/HashMap.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Abdellatif on 9/28/2016.
+ */
+public class HashMap {
+    private String[] keys;
+    private Object[] values;
+    private int size;
+
+    //Construct 
+    public HashMap(int size){
+        keys = new String[size];
+        values = new Object[size];
+    }
+
+    public boolean set(String key, Object value){
+        for int
+    }
+}
